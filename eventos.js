c6c787a98668a83ae0dc3d360c1ba6a9f74a2ac7
beluway <morenosuarezmariabelen@gmail.com>
2025-08-13 17:53:13 -0300

    solución final

--- eventos.js
+++ eventos.js
@@ -1,22 +1,23 @@
 
 addEventListener("DOMContentLoaded",function(){
-    
-    const BOTON = document.querySelector("#saludar");
 
-    //click en el botón
-        BOTON.addEventListener("click",function()
-        {alert("Hola! Soy el botón");}
-    
-    );
-
-    //no me quedó claro si era hacer click en el div o en el botón
-    //por las dudas hice ambos
-
-    const DIV = this.document.getElementById("div");
+const DIV = this.document.getElementById("div");
     //click en el div
     DIV.addEventListener("click",function(){
         {alert("Hola! Soy el div");}
     })
+
+    
+const BOTON = document.querySelector("#saludar");
+
+    //click en el botón
+        BOTON.addEventListener("click",function(event){
+            event.stopPropagation(); //evita que el evento del div se dispare en el objeto hijo
+    });
+
+
+
+    
 });
 
 
